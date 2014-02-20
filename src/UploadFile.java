a7269b340d9252aa34045393e11e5ba9e21d0e76
Fedmich <fedmich@gmail.com>
2014-02-20 14:30:38 +0800

    TransferManager code

--- src/UploadFile.java
+++ src/UploadFile.java
@@ -1,4 +1,34 @@
+import java.io.File;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
 
 public class UploadFile {
 
+
+    public static void main(String[] args) throws Exception {
+        String existingBucketName = "*** Provide existing bucket name ***";
+        String keyName            = "*** Provide object key ***";
+        String filePath           = "*** Provide file to upload ***";  
+        
+        TransferManager tm = new TransferManager(new PropertiesCredentials(
+        		UploadFile.class.getResourceAsStream(
+        				                   "AwsCredentials.properties")));        
+
+        // TransferManager processes all transfers asynchronously, 
+        // so this call will return immediately.
+        Upload upload = tm.upload(
+        		existingBucketName, keyName, new File(filePath));
+
+        try {
+        	// Or you can block and wait for the upload to finish
+        	upload.waitForCompletion();
+        } catch (AmazonClientException amazonClientException) {
+        	System.out.println("Unable to upload file, upload was aborted.");
+        	amazonClientException.printStackTrace();
+        }
+    }
 }
+
